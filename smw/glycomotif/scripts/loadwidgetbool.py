d35e500ae8ab196f3516e32dbe958e4b334dca65
hendricks <wenjinzhang95@gmail.com>
2018-12-14 11:02:36 -0800

    add viewer

--- smw/glycomotif/scripts/loadwidgetbool.py
+++ smw/glycomotif/scripts/loadwidgetbool.py
@@ -4,13 +4,13 @@ import json
 from getwiki import GlycoMotifWiki
 w = GlycoMotifWiki()
 
-from pygly.GlyTouCan import GlyTouCan
-gtc = GlyTouCan()
-
 brokenImage = json.loads(open("brokenimage.json").read())
 for m in w.itermotif():
     if m.get("collection") == "GM" and m.get("glytoucan") not in brokenImage:
+        print m.get("id")
         m.set("displayhgv", True)
+        w.update(m)
     else:
+        # continue
         m.set("displayhgv", False)
-    w.update(m)
+        w.update(m)
