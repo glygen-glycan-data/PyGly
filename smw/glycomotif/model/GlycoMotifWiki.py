e89dea38efb5263f3b30fd4482f984f76a16c8ea
hendricks <wenjinzhang95@gmail.com>
2018-12-16 16:10:39 -0800

    add viewer

--- smw/glycomotif/model/GlycoMotifWiki.py
+++ smw/glycomotif/model/GlycoMotifWiki.py
@@ -44,50 +44,48 @@ class Motif(SMW.SMWClass):
         return "%s.%s"%(kwargs.get('collection'),kwargs.get('accession'))
 
     def asaglycon(self,value):
-	if value not in Motif.aglyconvalues:
-	    raise ValueError("Bad aglycon value: %r"%(value,))
-	return value
+        if value not in Motif.aglyconvalues:
+            raise ValueError("Bad aglycon value: %r"%(value,))
+        return value
 
     def toPython(self,data):
-	data = super(Motif,self).toPython(data)
+        data = super(Motif,self).toPython(data)
 	
-	# name is newline separated
+        # name is newline separated
         if isinstance(data.get('name'),basestring):
             data['name'] = map(lambda s: s.strip(),data.get('name').split('\n'))
 
-	# aglycon is comma separated, with specific possible values, sorted, so behaves as set
+        # aglycon is comma separated, with specific possible values, sorted, so behaves as set
         if isinstance(data.get('aglycon'),basestring):
             data['aglycon'] = sorted(map(self.asaglycon,map(lambda s: s.strip(),data.get('aglycon').split(','))))
-	elif data.get('aglycon') != None:
-	    data['aglycon'] = sorted(map(self.asaglycon,data.get('aglycon')))
+        elif data.get('aglycon') != None:
+            data['aglycon'] = sorted(map(self.asaglycon,data.get('aglycon')))
 
-	# sameas is comma separated
+        # sameas is comma separated
         if isinstance(data.get('sameas'),basestring):
             data['sameas'] = sorted(map(lambda s: s.strip(),data.get('sameas').split(',')))
 
-    # topology is comma separated
+        # topology is comma separated
         if isinstance(data.get('topology'), basestring):
             data['topology'] = map(lambda s: s.strip(), data.get('topology').split(','))
 
-        
+        # redend is a list of booleans, sorted, so behaves as set
+        if isinstance(data.get('redend'),basestring):
+                data['redend'] = sorted(map(self.asboolean,map(lambda s: s.strip(),data.get('redend').split(','))))
+        elif data.get('redend') in (True,False):
+            data['redend'] = [ data.get('redend') ]
+        elif data.get('redend') != None:
+            data['redend'] = sorted(map(self.asboolean,data.get('redend')))
 
-	# redend is a list of booleans, sorted, so behaves as set
-	if isinstance(data.get('redend'),basestring):
-            data['redend'] = sorted(map(self.asboolean,map(lambda s: s.strip(),data.get('redend').split(','))))
-	elif data.get('redend') in (True,False):
-	    data['redend'] = [ data.get('redend') ]
-	elif data.get('redend') != None:
-	    data['redend'] = sorted(map(self.asboolean,data.get('redend')))
+        # Strip <pre> and </pre> if it is there
+        # if 'wurcs' in data:
+            #     data['wurcs'] = data.get('wurcs').lstrip('<pre>').rstrip('</pre>')
 
-	# Strip <pre> and </pre> if it is there 
-	# if 'wurcs' in data:
-        #     data['wurcs'] = data.get('wurcs').lstrip('<pre>').rstrip('</pre>')
+        # Strip <pre> and </pre> if it is there
+        # if 'glycoct' in data:
+            #     data['glycoct'] = data.get('glycoct').lstrip('<pre>').rstrip('</pre>')
 
-	# Strip <pre> and </pre> if it is there 
-	# if 'glycoct' in data:
-        #     data['glycoct'] = data.get('glycoct').lstrip('<pre>').rstrip('</pre>')
-
-	return data
+        return data
 
     def toTemplate(self,data):
         data = super(Motif,self).toTemplate(data)
@@ -107,13 +105,11 @@ class Motif(SMW.SMWClass):
         if 'topology' in data:
             data['topology'] = ",".join(data['topology'])
 
+        # if 'wurcs' in data:
+        #     data['wurcs'] = "<pre>" + data['wurcs'] + "</pre>"
 
-
-    # if 'wurcs' in data:
-	#     data['wurcs'] = "<pre>" + data['wurcs'] + "</pre>"
-	    
-	# if 'glycoct' in data:
-	#     data['glycoct'] = "<pre>" + data['glycoct'] + "</pre>"
+        # if 'glycoct' in data:
+        #     data['glycoct'] = "<pre>" + data['glycoct'] + "</pre>"
 	    
 	    return data
 
--- smw/glycomotif/scripts/loadwidgetbool.py
+++ smw/glycomotif/scripts/loadwidgetbool.py
@@ -7,10 +7,10 @@ w = GlycoMotifWiki()
 brokenImage = json.loads(open("brokenimage.json").read())
 for m in w.itermotif():
     if m.get("collection") == "GM" and m.get("glytoucan") not in brokenImage:
-        print m.get("id")
+        print "%s set to display" % m.get("id")
         m.set("displayhgv", True)
         w.update(m)
     else:
-        # continue
+        print "%s set to NOT display" % m.get("id")
         m.set("displayhgv", False)
         w.update(m)
