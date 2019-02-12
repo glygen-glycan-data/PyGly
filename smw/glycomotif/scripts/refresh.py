278a6cb853f721f234c1361294b979ac1178a84c
Nathan Edwards <edwardsnj@gmail.com>
2019-02-12 11:11:42 -0500

    more glycomotif scripts

--- smw/glycomotif/scripts/refresh.py
+++ smw/glycomotif/scripts/refresh.py
@@ -7,9 +7,23 @@ w = GlycoMotifWiki()
 
 if len(sys.argv) > 1:
 
-  for p in w.iterpages(regex=sys.argv[1]):
-    print >>sys.stderr, p.name
-    w.refresh(p)
+  if sys.argv[1] == "-":
+
+    for p in w.iterpages(exclude_categories=['Motif','Collection']):
+      print >>sys.stderr, p.name
+      w.refresh(p)
+
+  elif sys.argv[1] == "Collections":
+
+    for p in w.iterpages(include_categories=['Collection']):
+      print >>sys.stderr, p.name
+      w.refresh(p)
+
+  else:
+
+    for p in w.iterpages(regex=sys.argv[1]):
+      print >>sys.stderr, p.name
+      w.refresh(p)
 
 else:
 
