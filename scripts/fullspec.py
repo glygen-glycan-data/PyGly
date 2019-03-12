ddab8cc2f3a5d1b4e1caba793de64342883292bc
Nathan Edwards <edwardsnj@gmail.com>
2019-03-12 12:34:31 -0400

    More robust script for writing all fully specified glycans

--- scripts/fullspec.py
+++ scripts/fullspec.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python27
 
-import sys
+import sys, os, os.path
 import findpygly
 from pygly.GlyTouCan import GlyTouCan
+from pygly.GlycanFormatter import GlycoCTFormat
 
+glycoct_format = GlycoCTFormat()
 gtc = GlyTouCan()
 
 for l in sys.stdin:
     acc = l.strip()
-    if gtc.getGlycan(acc).fully_determined():
-	seq = gtc.getseq(acc,'glycoct')
-	if seq and not os.path.exists('%s.txt'%(acc,)):
-	    wh = open('%s.txt'%(acc,),'wb')
-	    wh.write(seq)
-	    wh.close()
+    g =  gtc.getGlycan(acc)
+    if g and g.fully_determined():
+	print acc,True
+	if not os.path.exists('%s.txt'%(acc,)):
+	    seq = gtc.getseq(acc,'glycoct')
+	    if not seq:
+		try:
+		    seq = glycoct_format.toStr(g)
+		except:
+		    pass
+	    if seq:
+	        wh = open('%s.txt'%(acc,),'w')
+	        wh.write(seq.strip()+'\n')
+	        wh.close()
+    else:
+	print acc,(False if g else None)
