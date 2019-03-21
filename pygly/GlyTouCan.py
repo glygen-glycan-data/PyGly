e2f4a7945e2cb34e55190f95e6ecf62f3933ed9b
Lihua Zhang <sarahzhang585@gmail.com>
2019-03-21 15:24:43 -0400

    xref change back to lower cases

--- pygly/GlyTouCan.py
+++ pygly/GlyTouCan.py
@@ -472,7 +472,7 @@ class GlyTouCan(object):
              ?saccharide glycan:has_resource_entry ?gdb
         } 
     """
-    resources = ['GLYCOSCIENCES.de','PubChem','KEGG','UniCarbKB','GlyConnect','GlycomeDB','Carbbank(CCSB)']
+    resources = ['glycosciences_de','pubchem','kegg','unicarbkb','glyconnect','glycome-db','carbbank']
     def getcrossrefs(self,accession,resource=None):
         assert resource in [None]+self.resources
 	response = self.query(self.getcrossrefs_sparql%dict(accession=accession))
