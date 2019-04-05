bf774db3376a230a799edeb9eaeb048da89f1dd1
Lihua Zhang <sarahzhang585@gmail.com>
2019-04-05 13:31:05 -0400

    change add_annotation to set_annotation

--- smw/glycandata/scripts/loadgtc.py
+++ smw/glycandata/scripts/loadgtc.py
@@ -17,24 +17,24 @@ for gtcacc in open(sys.argv[1]):
 	       wurcs=gtc.getseq(gtcacc,'wurcs'),
 	       glycoct=gtc.getseq(gtcacc,'glycoct'),
                iupac=gtc.getseq(gtcacc,'iupac_extended'))
-    g.add_annotation(value=gtc.getmass(gtcacc),
+    g.set_annotation(value=gtc.getmass(gtcacc),
                      property='UnderivitizedMW',
                      source='GlyTouCan',type='MolWt')
     try:
         mw = gtc.getGlycan(gtcacc).underivitized_molecular_weight()
-        g.add_annotation(value=mw,
+        g.set_annotation(value=mw,
 			 property='UnderivitizedMW',
 			 source='EdwardsLab', type='MolWt')
     except:
         pass
     try:
         pmw = gtc.getGlycan(gtcacc).permethylated_molecular_weight()
-        g.add_annotation(value=pmw,
+        g.set_annotation(value=pmw,
 			 property='PermethylatedMW',
 			 source='EdwardsLab', type='MolWt')
     except:
         pass
-    g.add_annotation(value=gtc.getmonocount(gtcacc),
+    g.set_annotation(value=gtc.getmonocount(gtcacc),
 		             property='MonosaccharideCount',
 		             source='GlyTouCan',type='MonosaccharideCount')
     try: 
@@ -43,11 +43,11 @@ for gtcacc in open(sys.argv[1]):
             count = comp[ckey]
             if count > 0:
 		if ckey=='Count':
-		    g.add_annotation(value=count,
+		    g.set_annotation(value=count,
 		         property='MonosaccharideCount',
 		         source='EdwardsLab',type='MonosaccharideCount')
 		else:
-	            g.add_annotation(value=count,
+	            g.set_annotation(value=count,
 		        property=ckey+'Count',
 		        source='EdwardsLab',type='MonosaccharideCount')
     except:
@@ -66,44 +66,44 @@ for gtcacc in open(sys.argv[1]):
 	dic.setdefault(ref,[]).append(c)
     for key in dic:       
         if key in xref_dic:    
-            g.add_annotation(value=dic[key],
+            g.set_annotation(value=dic[key],
                 property=xref_dic[key],
                 source='GlyTouCan',type='CrossReference')
         else:	
-            g.add_annotation(value=dic[key],
+            g.set_annotation(value=dic[key],
                 property=key,
                 source='GlyTouCan',type='CrossReference')
     motif_list = []	
     for motif in list(gtc.getmotif(gtcacc)):
         acc, name = motif
 	motif_list.append(acc)
-        g.add_annotation(value=motif_list,
+        g.set_annotation(value=motif_list,
                 property='Motif',
                 source='GlyTouCan', type='Motif')
     topo = gtc.gettopo(gtcacc)
     if topo:
-        g.add_annotation(value=topo,
+        g.set_annotation(value=topo,
                 property='Topology',
 		source='GlyTouCan', type='Subsumption')
     comp = gtc.getcomp(gtcacc)
     if comp:
-        g.add_annotation(value=comp,
+        g.set_annotation(value=comp,
                 property='Composition',
 		source='GlyTouCan', type='Subsumption')
     if gtcacc == 'G00031MO':
-	g.add_annotation(value='O-linked',
+	g.set_annotation(value='O-linked',
 			 property='GlycanType',
 		 	 source='EdwardsLab',
 			 type='Classification',
 			 method='Glycan Type by Motif Match',
 			 reference='https://glytoucan.org/Structures/Glycans/G00032MO')
-	g.add_annotation(value='core 1',
+	g.set_annotation(value='core 1',
 			 property='GlycanSubtype',
 		 	 source='EdwardsLab',
 			 type='Classification',
 			 method='Glycan Type by Motif Match',
 			 reference='https://glytoucan.org/Structures/Glycans/G00032MO')
-    if w.put(g):
+    if w.update(g):
 	print >>sys.stderr, g.get('accession')
     current.add(gtcacc)
 
