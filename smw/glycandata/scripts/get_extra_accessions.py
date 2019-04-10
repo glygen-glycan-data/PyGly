2d00a7f5c3fb6db5ff6e961508723fdc0718e1fa
Lihua Zhang <lzhang@codon.edwardslab.bmcb.georgetown.edu>
2019-04-10 15:14:28 -0400

    get extra accessions finished

---
+++ smw/glycandata/scripts/get_extra_accessions.py
@@ -0,0 +1,30 @@
+import sys
+
+from getwiki import GlycanDataWiki, Glycan
+
+w = GlycanDataWiki()
+
+from pygly.GlyTouCan import GlyTouCan
+
+gtc = GlyTouCan()
+
+accs = set(open('../data/glytoucan_accessions.txt').read().split())
+newaccs = set()
+
+for acc in accs:
+    topo = gtc.gettopo(acc)
+    comp = gtc.getcomp(acc)
+    if topo:
+        newaccs.add(topo)
+    if comp:
+        for newacc in gtc.hascomp(comp)
+            newaccs.add(newacc)    
+        newaccs.add(comp)
+newaccs = (newaccs-accs)
+
+wh = open('../data/extra_accessions.txt','w')
+for acc in sorted(newaccs):
+    print >> wh, acc
+wh.close()
+
+
