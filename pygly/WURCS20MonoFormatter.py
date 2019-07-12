a6b6541b6f90f36ee20841260086c789dd5413c9
Hendricks27 <wenjinzhang95@gmail.com>
2019-07-12 17:26:40 -0400

    add support for double linked substituent like pyruvate

--- pygly/WURCS20MonoFormatter.py
+++ pygly/WURCS20MonoFormatter.py
@@ -7,47 +7,53 @@ from Monosaccharide import *
 # This line import the package mutually, be careful
 from GlycanFormatter import WURCS20ParseError
 
+
 class InvalidMonoError(WURCS20ParseError):
-    def __init__(self,monostr):
-        self.message = "WURCS2.0 parser: Invalid monosaccharide: %s"%(monostr,)
+    def __init__(self, monostr):
+        self.message = "WURCS2.0 parser: Invalid monosaccharide: %s" % (monostr,)
 
     def __str__(self):
         return self.message
+
 
 class UnsupportedMonoError(WURCS20ParseError):
-    def __init__(self,monostr):
-        self.message = "WURCS2.0 parser: Unsupported monosaccharide: %s"%(monostr,)
+    def __init__(self, monostr):
+        self.message = "WURCS2.0 parser: Unsupported monosaccharide: %s" % (monostr,)
 
     def __str__(self):
         return self.message
+
 
 class UnsupportedSkeletonCodeError(UnsupportedMonoError):
     def __init__(self, skel):
-        self.message = "WURCS2.0 parser: Unsupported skeleton code: %s"%(skel,)
+        self.message = "WURCS2.0 parser: Unsupported skeleton code: %s" % (skel,)
+
 
 class UnsupportedSubstituentError(UnsupportedMonoError):
     def __init__(self, sub):
-        self.message = "WURCS2.0 parser: Unsupported substituent: %s"%(sub,)
+        self.message = "WURCS2.0 parser: Unsupported substituent: %s" % (sub,)
+
 
 class WURCS20MonoFormat:
-
-    mono_pattern = re.compile(r"^([0-9a-zA-Z]{3,9})((-\d[abx])(_[0-9?]-[0-9?])?)?((_([0-9?]|[0-9]\|[0-9]|[0-9]-[0-9])(\*[^_]+)?)*)$")
+    mono_pattern = re.compile(
+        r"^([0-9a-zA-Z]{3,9})((-\d[abx])(_[0-9?]-[0-9?])?)?((_([0-9?]|[0-9]\|[0-9]|[0-9]-[0-9])(\*[^_]+)?)*)$")
 
     def __init__(self):
         self.cache = {}
         self.load()
 
     def load(self):
-	skelconfigfile = resource_stream(__name__, 'wurcs20_skeleton.ini')
+        skelconfigfile = resource_stream(__name__, 'wurcs20_skeleton.ini')
         self.skelconfig = ConfigParser.SafeConfigParser()
         self.skelconfig.readfp(skelconfigfile)
 
-	subsconfigfile = resource_stream(__name__, 'wurcs20_substituent.ini')
+        subsconfigfile = resource_stream(__name__, 'wurcs20_substituent.ini')
         self.subsconfig = ConfigParser.SafeConfigParser()
         self.subsconfig.readfp(subsconfigfile)
 
     skel_config_get_default_section = ""
-    def skel_config_get(self,optionx,skelton_code = skel_config_get_default_section):
+
+    def skel_config_get(self, optionx, skelton_code=skel_config_get_default_section):
         try:
             return self.skelconfig.get(self.skel_config_get_default_section, optionx)
         except ConfigParser.NoOptionError:
@@ -65,7 +71,7 @@ class WURCS20MonoFormat:
         ring = parsed.group(4)
         substs = parsed.group(5)
 
-	# print mono_string
+        # print mono_string
         # print skel, anomer, ring, substs
         m = Monosaccharide()
 
@@ -78,19 +84,19 @@ class WURCS20MonoFormat:
 
             stem = self.skel_config_get("stem")
             if stem:
-		m.set_stem(*(map(lambda s: eval(s),stem.split())))
+                m.set_stem(*(map(lambda s: eval(s), stem.split())))
 
             config = self.skel_config_get("config")
             if config:
-		m.set_config(*(map(lambda c: eval(c),config.split())))
-	    else:
-	        if stem:
-		    m.set_config(*([None]*len(m.stem())))
+                m.set_config(*(map(lambda c: eval(c), config.split())))
+            else:
+                if stem:
+                    m.set_config(*([None] * len(m.stem())))
 
-	    if m.stem() and m.config():
-		assert len(m.stem()) == len(m.config()), "Inconsistent config/stem for %s"%(skel,)
-	    elif not m.stem():
-		assert not m.config(), "Problem with config/stem for %s"(skel,)
+            if m.stem() and m.config():
+                assert len(m.stem()) == len(m.config()), "Inconsistent config/stem for %s" % (skel,)
+            elif not m.stem():
+                assert not m.config(), "Problem with config/stem for %s"(skel, )
 
             superclass = self.skel_config_get("superclass")
             if superclass:
@@ -127,47 +133,57 @@ class WURCS20MonoFormat:
         if substs:
             sub_list = substs[1:].split("_")
             for sub in sub_list:
-		try:
-                    pp,sub_name = sub.split("*")
-		except ValueError:
-		    # missing *, no sub_name, coresponds to pp = 
-		    pp = sub; sub_name = "anhydro"
-		    # Note, we appear to require \d-\d for sub, otherwise it is a no-op?
-		    # cases with \d only do not appear to change the mass...
-		    if not re.search(r'^\d-\d$',sub):
-			raise InvalidMonoError(mono_string)
                 try:
-                    sub_type = self.subsconfig.get(sub_name,"type")
-                    pt = self.subsconfig.get(sub_name,"parent_type")
-                    ct = self.subsconfig.get(sub_name,"child_type")
-                    cp = self.subsconfig.get(sub_name,"child_pos")
-		    if cp != None:
-			cp = int(cp)
+                    pp, sub_name = sub.split("*", 1)
+                except ValueError:
+                    # missing *, no sub_name, coresponds to pp =
+                    pp = sub
+                    sub_name = "anhydro"
+                    # Note, we appear to require \d-\d for sub, otherwise it is a no-op?
+                    # cases with \d only do not appear to change the mass...
+                    if not re.search(r'^\d-\d$', sub):
+                        raise InvalidMonoError(mono_string)
+                try:
+                    sub_type = self.subsconfig.get(sub_name, "type")
+                    pt = self.subsconfig.get(sub_name, "parent_type")
+                    ct = self.subsconfig.get(sub_name, "child_type")
+                    cp = self.subsconfig.get(sub_name, "child_pos")
+                    if cp != None:
+                        cp = int(cp)
 
-                    if '|' not in pp and '-' not in pp:
-                        if pp=="?" or pp=="-1":
+                    if re.search(r"^\d-\d$", pp):
+                        subins = Substituent(eval(sub_type))
+                        for pp in pp.split("-"):
+                            m.add_substituent(subins, parent_pos=pp, parent_type=eval(pt), child_pos=cp,
+                                              child_type=eval(ct))
+
+                    elif '|' not in pp and '-' not in pp:
+                        if pp == "?" or pp == "-1":
                             pp = -1
-			else:
-			    pp = int(pp)
-                        m.add_substituent(eval(sub_type),parent_pos=pp,parent_type=eval(pt),child_pos=cp,child_type=eval(ct))
+                        else:
+                            pp = int(pp)
+                        m.add_substituent(eval(sub_type), parent_pos=pp, parent_type=eval(pt), child_pos=cp,
+                                          child_type=eval(ct))
                     elif '|' in pp:
-                        pp = map(int,pp.split('|'))
-                        m.add_substituent(eval(sub_type),parent_pos=pp,parent_type=eval(pt),child_pos=cp,child_type=eval(ct))
-		    elif '-' in pp and sub_name == "anhydro":
-			pp = map(int,pp.split('-'))
-			if len(pp) != 2:
-			    raise  UnsupportedSubstituentError(sub)
-			subst = m.add_substituent(eval(sub_type),parent_pos=pp[0],parent_type=eval(pt),
-						 child_pos=cp,child_type=eval(ct)).child()
-			pt = self.subsconfig.get(sub_name+"1","parent_type")
-                        ct = self.subsconfig.get(sub_name+"1","child_type")
-                        cp = self.subsconfig.get(sub_name+"1","child_pos")
-		        if cp != None:
-			    cp = int(cp)
-			m.add_substituent(subst,parent_pos=pp[1],parent_type=eval(pt),child_pos=cp,child_type=eval(ct))
-		    else:
-			raise UnsupportedSubstituentError(sub)
-                except (ConfigParser.NoSectionError,ValueError):
+                        pp = map(int, pp.split('|'))
+                        m.add_substituent(eval(sub_type), parent_pos=pp, parent_type=eval(pt), child_pos=cp,
+                                          child_type=eval(ct))
+                    elif '-' in pp and sub_name == "anhydro":
+                        pp = map(int, pp.split('-'))
+                        if len(pp) != 2:
+                            raise UnsupportedSubstituentError(sub)
+                        subst = m.add_substituent(eval(sub_type), parent_pos=pp[0], parent_type=eval(pt),
+                                                  child_pos=cp, child_type=eval(ct)).child()
+                        pt = self.subsconfig.get(sub_name + "1", "parent_type")
+                        ct = self.subsconfig.get(sub_name + "1", "child_type")
+                        cp = self.subsconfig.get(sub_name + "1", "child_pos")
+                        if cp != None:
+                            cp = int(cp)
+                        m.add_substituent(subst, parent_pos=pp[1], parent_type=eval(pt), child_pos=cp,
+                                          child_type=eval(ct))
+                    else:
+                        raise UnsupportedSubstituentError(sub)
+                except (ConfigParser.NoSectionError, ValueError):
                     # Substituent not supported or badly formatted
                     raise UnsupportedSubstituentError(sub)
 
@@ -191,10 +207,10 @@ if __name__ == "__main__":
     mf = WURCS20MonoFormat()
     args = False
     for w in sys.argv[1:]:
-	args = True
-	print >>sys.stderr, w
-	print mf.get(w)
+        args = True
+        print >> sys.stderr, w
+        print mf.get(w)
     if not args:
         for l in sys.stdin:
-	    print >>sys.stderr, l.strip()
-	    print mf.get(l.strip())
+            print >> sys.stderr, l.strip()
+            print mf.get(l.strip())
