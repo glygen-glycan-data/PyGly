f269462eaff13321695a045d47800fa225cd1062
Nathan Edwards <edwardsnj@gmail.com>
2019-07-23 11:30:33 -0400

    partition image zip files to bring it under the 100MB github cap

--- smw/glycandata/scripts/dlimages.sh
+++ smw/glycandata/scripts/dlimages.sh
@@ -7,11 +7,13 @@ cat "$@" | (cd images/cfg/normal;  $DIR/getimages.py cfg normal )
 cat "$@" | (cd images/cfg/compact; $DIR/getimages.py cfg compact )
 cat "$@" | (cd images/cfg/extended; $DIR/getimages.py cfg extended )
 cd images
-rm -f ../images.zip
-zip -q -r "../images.zip" cfg
+rm -f ../images*.zip
+zip -9 -q -r "../images-cfg-extended.zip" cfg/extended
+zip -9 -q -r "../images-cfg-normal" cfg/normal
+zip -9 -q -r "../images-cfg-compact" cfg/compact
 echo "GlyTouCanAccession" > "../images.txt"
 find cfg -name "*.png" \! -empty | sed -n 's/\.png$//p' | sed 's/^.*\///' | sort -u >> ../images.txt
 cd ..
 echo -e "GlyTouCanAccession\tImage-Size\tImage-CRC\tImage-Notation\tImage-Style" > "images-crc.txt"
-unzip -lv images.zip | awk '{print $1,$7,$8}' | tr '/.' '  ' | awk '{print $5,$1,$2,$3,$4}' | awk 'NF == 5' | egrep -v '(00000000|17981711)' | sort -k5,5 -k1,1 | tr ' ' '\t' >> images-crc.txt
+( unzip -lv images-cfg-extended.zip ; unzip -lv images-cfg-normal.zip ; unzip -lv images-cfg-compact.zip ) | awk '{print $1,$7,$8}' | tr '/.' '  ' | awk '{print $5,$1,$2,$3,$4}' | awk 'NF == 5' | egrep -v '(00000000|17981711)' | sort -k5,5 -k1,1 | tr ' ' '\t' >> images-crc.txt
 $DIR/checkimg.py images-crc.txt
