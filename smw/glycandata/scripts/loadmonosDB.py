7a57a320341e6e986cc23a7f371c18bd7d15fcc4
Lihua Zhang <lzhang@dalton.edwardslab.bmcb.georgetown.edu>
2019-08-02 10:57:17 -0400

    loadmonosDB from allmonosaccharideDB.tsv file

--- smw/glycandata/scripts/loadmonosDB.py
+++ smw/glycandata/scripts/loadmonosDB.py
@@ -4,28 +4,36 @@ import sys
 from collections import defaultdict
 
 from getwiki import GlycanData, Glycan
+from pygly.GlycanFormatter import GlycoCTFormat
+
 w = GlycanData()
+glycoctformat = GlycoCTFormat()
 
+monosdb = {}
+f = open(sys.argv[1],'r')
+for line in f:
+    k,v = line.split()
+    monosdb[k] = v
 
-def monosDB(infile):
-    d = defaultdict(set)
-    f = open(infile,'r')
-    
-    for line in f:
-	if "Some monosaccharide" in line or "MonosaccharideDB_ID" in line:
-	    continue
-	k,v = line.split()
-	d[k].add(v)
-    return d
-
-monosdb = monosDB(sys.argv[1])
-
-for m in w.iterglycan():
-    acc = m.get('accession')
-    m.delete_annotations(source="EdwardsLab",property="MonosaccharideDB",type="CrossReference")
-    if acc in monosdb:
-        m.set_annotation(value=list(monosdb[acc]), property="MonosaccharideDB",source="EdwardsLab",type="CrossReference")
-    if w.put(m):
+for g in w.iterglycan():
+    monosdic = defaultdict(set)
+    acc = g.get('accession')
+    glycan = g.getGlycan()
+    if not glycan:
+        continue
+    for m in glycan.all_nodes():
+        try:
+            glycoctsym = glycoctformat.mtoStr(m)
+        except KeyError:
+            print acc,"-"
+            continue
+        try:
+            monosdic[acc].add(monosdb[glycoctsym])
+        except KeyError:
+            continue
+    g.delete_annotations(source="EdwardsLab",property="MonosaccharideDB",type="CrossReference")
+    g.set_annotation(value=list(monosdic[acc]), property="MonosaccharideDB",source="EdwardsLab",type="CrossReference")
+    if w.put(g):
         print acc
 
 
