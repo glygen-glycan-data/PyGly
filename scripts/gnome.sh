ec59744dc8eaed104f72d5e62c9ef4323a91937f
Hendricks27 <wenjinzhang95@gmail.com>
2019-09-21 23:40:31 -0400

    Python interpreter version change

--- scripts/gnome.sh
+++ scripts/gnome.sh
@@ -13,8 +13,8 @@ if [ -z "$1" ]
 fi
 
 git clone git@github.com:glygen-glycan-data/GNOme.git
-python ../pygly/GNOme.py writeowl ./GNOme/data/gnome_subsumption_raw.txt ./GNOme.owl ./GNOme/data/mass_lookup_2decimal $1
-python ../pygly/GNOme.py viewerdata ./GNOme.owl ./GNOme.browser.js
+python27 ../pygly/GNOme.py writeowl ./GNOme/data/gnome_subsumption_raw.txt ./GNOme.owl ./GNOme/data/mass_lookup_2decimal $1
+python27 ../pygly/GNOme.py viewerdata ./GNOme.owl ./GNOme.browser.js
 
 #python ../pygly/GNOme.py writeresowl ./GNOme.owl BCSDB ./GNOme_BCSDB.owl
 #python ../pygly/GNOme.py writeresowl ./GNOme.owl GlyGen ./GNOme_GlyGen.owl
@@ -24,8 +24,8 @@ python ../pygly/GNOme.py viewerdata ./GNOme.owl ./GNOme.browser.js
 
 for Restriction_set in "BCSDB" "GlyGen"
 do
-  python ../pygly/GNOme.py writeresowl ./GNOme.owl $Restriction_set ./GNOme_$Restriction_set.owl
-  python ../pygly/GNOme.py viewerdata ./GNOme_$Restriction_set.owl ./GNOme_$Restriction_set.browser.js
+  python27 ../pygly/GNOme.py writeresowl ./GNOme.owl $Restriction_set ./GNOme_$Restriction_set.owl
+  python27 ../pygly/GNOme.py viewerdata ./GNOme_$Restriction_set.owl ./GNOme_$Restriction_set.browser.myopic.js
 done
 
 
@@ -35,7 +35,7 @@ mv ./GNOme.* ./GNOme/
 
 for Restriction_set in "BCSDB" "GlyGen"
 do
-	for file_ext in "owl" "obo" "json" "browser.js"
+	for file_ext in "owl" "obo" "json" "browser.myopic.js"
   do
       # touch ./GNOme_$Restriction_set.$file_ext
     	mv ./GNOme_$Restriction_set.$file_ext ./GNOme/restrictions/
