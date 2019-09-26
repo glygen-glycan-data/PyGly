86ced4b7f947a4bdae159393a40a63c415d30b36
Nathan Edwards <edwardsnj@gmail.com>
2019-09-26 12:55:28 -0400

    Update a few glycans to O-glycans, add table to main page

--- smw/gptwiki/scripts/getglycoct.py
+++ smw/gptwiki/scripts/getglycoct.py
@@ -8,7 +8,7 @@ from pygly.GlyTouCan import GlyTouCan
 import os, sys, urllib, string
 import Bio.SeqIO
 
-gtc = GlyTouCan(usecache=False)
+gtc = GlyTouCan(usecache=True)
 w = GPTWiki()
 try:
     os.mkdir('../glycoct')
@@ -20,8 +20,10 @@ for gc in sorted(w.iterglycans(),key=lambda gc:gc.get('accession')):
     topos = map(str.strip,map(str,gc.get('topo')))
     for tacc in topos:
         glycoct = gtc.getseq(tacc,'glycoct')
-        if glycoct:
-            f = open('../glycoct/' + acc + '.' + tacc + '.txt', 'w')
-            f.write(glycoct)
-            f.close() 
-            
+	if not glycoct:
+	    gly = gtc.getGlycan(tacc)
+	    glycoct = gly.glycoct()
+        f = open('../glycoct/' + acc + '.' + tacc + '.txt', 'w')
+        f.write(glycoct)
+        f.close() 
+	print >>sys.stderr, "Dump GlycoCT to %s.%s.txt"%(acc,tacc)
