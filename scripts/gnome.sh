ad222f5c80431f2305a178d00b41519ddad6c795
Hendricks27 <wenjinzhang95@gmail.com>
2019-09-27 13:31:48 -0400

    automation script update

--- scripts/gnome.sh
+++ scripts/gnome.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+restriction_set_names=(
+  "BCSDB"
+  "GlyGen"
+)
+
+
 if [[ ! ("$(pwd)" =~ "/PyGly/scripts") ]]; then
   # Check for executing folder
   exit
@@ -14,18 +20,13 @@ fi
 
 git clone git@github.com:glygen-glycan-data/GNOme.git
 python27 ../pygly/GNOme.py writeowl ./GNOme/data/gnome_subsumption_raw.txt ./GNOme.owl ./GNOme/data/mass_lookup_2decimal $1
-python27 ../pygly/GNOme.py viewerdata ./GNOme.owl ./GNOme.browser.myopic.js
+python27 ../pygly/GNOme.py viewerdata ./GNOme.owl ./GNOme/data/gnome_subsumption_raw.txt ./GNOme.browser.json
 
-#python ../pygly/GNOme.py writeresowl ./GNOme.owl BCSDB ./GNOme_BCSDB.owl
-#python ../pygly/GNOme.py writeresowl ./GNOme.owl GlyGen ./GNOme_GlyGen.owl
 
-#python ../pygly/GNOme.py viewerdata ./GNOme_BCSDB.owl ./GNOme_BCSDB.browser.js
-#python ../pygly/GNOme.py viewerdata ./GNOme_GlyGen.owl ./GNOme_GlyGen.browser.js
-
-for Restriction_set in "BCSDB" "GlyGen"
+for Restriction_set in "${restriction_set_names[@]}"
 do
   python27 ../pygly/GNOme.py writeresowl ./GNOme.owl $Restriction_set ./GNOme_$Restriction_set.owl
-  python27 ../pygly/GNOme.py viewerdata ./GNOme_$Restriction_set.owl ./GNOme_$Restriction_set.browser.myopic.js
+  python27 ../pygly/GNOme.py viewerdata ./GNOme_$Restriction_set.owl ./GNOme/data/gnome_subsumption_raw.txt ./GNOme_$Restriction_set.browser.json
 done
 
 
@@ -33,9 +34,9 @@ cp ./GNOme/convert.sh ./
 ./convert.sh
 mv ./GNOme.* ./GNOme/
 
-for Restriction_set in "BCSDB" "GlyGen"
+for Restriction_set in "${restriction_set_names[@]}"
 do
-	for file_ext in "owl" "obo" "json" "browser.myopic.js"
+	for file_ext in "owl" "obo" "json" "browser.json"
   do
       # touch ./GNOme_$Restriction_set.$file_ext
     	mv ./GNOme_$Restriction_set.$file_ext ./GNOme/restrictions/
