ec625775822b57e61d4a9a23f62113b243c4fe63
Nathan Edwards <edwardsnj@gmail.com>
2020-03-03 11:25:15 -0500

    update to basecomp name script

--- smw/glycandata/scripts/getbasecomplist.py
+++ smw/glycandata/scripts/getbasecomplist.py
@@ -19,6 +19,41 @@ basecomp = {'x-HEX-x:x':'Hex',
 	    'phosphate':'P',
 	    'sulfate':'S'}
 
+badskel = set("""
+axxxxh-1x
+""".split())
+
+# NeuAc, NeuGc, Fuc, Hex, HexNAc, dHex, Pent
+expskel = set("""
+  AUd21122h_5*NCC/3=O
+  AUd21122h_5*NCCO/3=O
+  u1221m
+  uxxxxh
+  uxxxxh_2*NCC/3=O
+  uxxxxm
+  uxxxh
+""".split())
+
+#
+# G43642TU and G06577JJ represent the same glycan - broken canonicalization - choose G06577JJ 
+# G43560PI and G94250TD represent the same glycan - broken canonicalization - choose G43560PI
+# G45924NL is floating in-link substituent - IMO this is not a correct representation
+# G99993XU ditto
+# G94401PZ ditto
+# G10633KT ditto
+# G48302UE ditto
+# 
+
+badacc = set("""
+G43642TU
+G94250TD
+G45924NL
+G99993XU
+G94401PZ
+G10633KT
+G48302UE
+""".split())
+
 # f = open('../data/basecomplist1.txt','w')
 
 gtc = GlyTouCan()
@@ -28,16 +63,32 @@ def accessions():
 	for acc in sys.stdin:
 	    yield acc.strip()
     elif len(sys.argv) == 2 and sys.argv[1] == "*":
-	for acc in gtc.allacc():
+	for acc in gtc.allaccessions():
 	    yield acc
     else:
 	for acc in sys.argv[1:]:
 	    yield acc
 
+allskel = set()
+
 for acc in accessions():
+
+    if acc in badacc:
+	continue
+
     glycan = gtc.getGlycan(acc,format='wurcs')
     if not glycan:
         continue
+
+    wurcs = gtc.getseq(acc,format='wurcs')
+    allgood = True
+    for skel in wurcs.split('/[', 1)[1].split(']/')[0].split(']['):
+	if skel in badskel:
+	    allgood = False
+	    break
+
+    if not allgood:
+	continue
 
     node_count = 0
     for m in glycan.all_nodes(undet_subst=True):
@@ -66,8 +117,13 @@ for acc in accessions():
     if comp['Fuc'] > 0 and comp['dHex'] > 0:
 	continue
 
+    newskel = set(wurcs.split('/[', 1)[1].split(']/')[0].split(']['))
+    if len(newskel - expskel) > 0:
+	print >>sys.stderr, "Unexpected skeleton codes:",", ".join(newskel - expskel)
+	sys.exit(1)
+
     byonic_good = False
-    if node_count == sum(map(lambda k: comp.get(k,0),("HexNAc","Hex","Fuc","dHex","NeuAc","NeuGc"))):
+    if node_count == sum(map(lambda k: comp.get(k,0),("HexNAc","Hex","Fuc","dHex","NeuAc","NeuGc","Pent","P","S"))):
 	byonic_good = True
 
     uckb_good = False
@@ -81,79 +137,69 @@ for acc in accessions():
     if not byonic_good and not uckb_good and not short_good:
 	continue
 
-    byonic_string = ''
-    similar_string = ''
-    full_string = ''
-    short_string = ''
+    if comp['Fuc'] > 0:
 
-    if comp['Fuc'] !=0:
-        for k in ['HexNAc','Hex','Fuc','NeuAc','NeuGc']:
-            if comp[k] != 0:
-                byonic_string += k + '(' + str(comp[k]) + ')'
-        byonic_line = acc + '\t' + byonic_string + "\tBYONIC\n"
-        if byonic_string:
-            print byonic_line.strip()
-            # f.write(byonic_line)
-        comp['H'] = comp['Hex']
-        comp['N'] = comp['HexNAc']
-        comp['F'] = comp['Fuc']
-        comp['Sia'] = comp['NeuAc'] + comp['NeuGc']
-        for k in ['H','N','F','Sia']:
-            if comp[k] != 0:
-                if comp[k] == 1:
-                    if k == 'Sia':
-                        short_string += 'S'
-                    else:
-                        short_string += k
-                else:
-                    if k == 'Sia':
-                        short_string += 'S' + str(comp['Sia'])
-                    else:
-                        short_string += k + str(comp[k])
-        short_line = acc + '\t' + short_string + '\tSHORTCOMP\n'
-        if short_good:
-            print short_line.strip()
-            # f.write(short_line)
-    else:
-        for k in ['HexNAc','Hex','dHex','NeuAc','NeuGc','Pent','P','S']:
-            if comp[k] != 0:
-                byonic_string += k + '(' + str(comp[k]) + ')'
-                similar_string += k + str(comp[k])
-        byonic_line = acc + '\t' + byonic_string + '\tBYONIC\n'
-        similar_line = acc + '\t' + similar_string + '\tSHORTUCKB\n'
         if byonic_good:
-            print byonic_line.strip()
-            # f.write(byonic_line)
-        if uckb_good:
-            print similar_line.strip()
-            # f.write(similar_line)
-        for k in ['KDN','HexA']:
-            comp[k] = 0
-        for k in ['HexNAc','Hex','dHex','NeuAc','NeuGc','Pent','S','P','KDN','HexA']:
-            full_string += k + str(comp[k])
-        full_line = acc + '\t' + full_string + '\tUCKBCOMP\n'
-        if uckb_good:
-            print full_line.strip()
-            # f.write(full_line)
-        if comp['dHex'] == 0:
+
+            byonic_string = ''
+            for k in ['HexNAc','Hex','Fuc','NeuAc','NeuGc','Pent','S','P']:
+                if comp[k] != 0:
+		    if k == 'P':
+                        byonic_string += 'Phospho' + '(' + str(comp[k]) + ')'
+		    elif k == 'S':
+                        byonic_string += 'Sulpho' + '(' + str(comp[k]) + ')'
+		    else:
+                        byonic_string += k + '(' + str(comp[k]) + ')'
+	    print "\t".join([acc,byonic_string,"BYONIC"])
+
+	if short_good:
+
+	    short_string = ''
             comp['H'] = comp['Hex']
             comp['N'] = comp['HexNAc']
-            comp['Sia'] = comp['NeuAc'] + comp['NeuGc']
-            for k in ['H','N','Sia']:
+            comp['F'] = comp['Fuc']
+            comp['Sia'] = comp['NeuAc']
+            for k in ['H','N','F','Sia']:
                 if comp[k] != 0:
-                    if comp[k] == 1:
-                        if k == 'Sia':
-                            short_string += 'S'
-                        else:
-                            short_string += k
-                    else:
-                        if k == 'Sia':
-                            short_string += 'S' + str(comp['Sia'])
-                        else:
-                            short_string += k + str(comp[k])
-            short_line = acc + '\t' + short_string + '\tSHORTCOMP\n'
-            if short_good:
-                print short_line.strip()
-                # f.write(short_line)
-# f.close()
+		    short_string += k[0]
+		    if comp[k] > 1:
+		        short_string += str(comp[k])
+	    print "\t".join([acc,short_string,"SHORTCOMP"])
+
+    else:
+
+	byonic_string = ''
+	similar_string = ''
+        for k in ['HexNAc','Hex','dHex','NeuAc','NeuGc','Pent','S','P']:
+            if comp[k] != 0:
+		if k == 'P':
+                    byonic_string += 'Phospho' + '(' + str(comp[k]) + ')'
+		elif k == 'S':
+                    byonic_string += 'Sulpho' + '(' + str(comp[k]) + ')'
+		else:
+                    byonic_string += k + '(' + str(comp[k]) + ')'
+                similar_string += k + str(comp[k])
+        if byonic_good:
+	    print "\t".join([acc,byonic_string,"BYONIC"])
+        if uckb_good:
+	    print "\t".join([acc,similar_string,"SHORTUCKB"])
+
+	full_string = ''
+        for k in ['HexNAc','Hex','dHex','NeuAc','NeuGc','Pent','S','P','KDN','HexA']:
+            full_string += k + str(comp[k])
+        if uckb_good:
+	    print "\t".join([acc,full_string,"UCKBCOMP"])
+
+        if short_good and comp['dHex'] == 0:
+
+	    short_string = ''
+            comp['H'] = comp['Hex']
+            comp['N'] = comp['HexNAc']
+            comp['Sia'] = comp['NeuAc']
+            for k in ['H','N','Sia']:
+                if comp[k] > 0:
+	    	    short_string += k[0]
+		    if comp[k] > 1:
+		        short_string += str(comp[k])
+            print "\t".join([acc,short_string,"SHORTCOMP"])
 
