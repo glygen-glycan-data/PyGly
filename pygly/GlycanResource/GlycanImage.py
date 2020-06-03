0b14a7b9ed00feeea11e98972c5343580019314d
Nathan Edwards <edwardsnj@gmail.com>
2020-06-03 12:51:59 -0400

    GlycanResource classes for GlycoMotif

---
+++ pygly/GlycanResource/GlycanImage.py
@@ -0,0 +1,21 @@
+
+try:
+    from pygly.GlycanImage import GlycanImage as GI
+except:
+    from GlycanImage import GlycanImage as GI
+
+from GlyTouCan import GlyTouCan
+
+class GlycanImage(object):
+    def __init__(self):
+	self.imageWriter = GI()
+	self.gtc = GlyTouCan(prefetch=False)
+    def write(self,*args,**kw):
+	for k,v in kw.items():
+	    self.imageWriter(k,v)
+	self.imageWriter.set('format','png')
+	for acc in args:
+	    glycoct = self.gtc.glycoct(acc)
+	    if not glycoct:
+	        continue
+            self.imageWriter.writeImage(glycoct,acc+".png")
