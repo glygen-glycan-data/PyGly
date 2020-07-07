7c725a07524883b2c87c58dbbdb0115b7d1898fd
Lihua Zhang <lzhang@maldi.edwardslab.bmcb.georgetown.edu>
2020-07-07 14:00:54 -0400

    implemented the IteratedRobustLinearRegression Class

--- smw/gptwiki/analysis/regression.py
+++ smw/gptwiki/analysis/regression.py
@@ -9,26 +9,27 @@ class LinearRegression(object):
 
     def __init__(self,**kw):
 	self._minpoints = kw.get('minpoints',2)
-
+    
     def normalize(self,points):
 	self.check(points)
 	apoints = np.array(points)
-	ind = np.argsort(apoints[:,0])
-	x = apoints[ind,0]
-	y = apoints[ind,1]
-	invind = np.argsort(ind)
+        ind = np.argsort(apoints[:,0])
+        x = apoints[ind,0]
+        y = apoints[ind,1]
+        invind = np.argsort(ind)
         return x,y,invind
 
     def check(self,points):
 	if len(points) < self._minpoints:
 	    raise ValueError("Too few points for linear regression")
-
+    
     def evaluate(self,params,points):
-	x,y,ind = self.normalize(points)
-	yfit = params['slope']*x+params['intercept']
-	return dict(yfit=yfit[ind],residuals=yfit[ind]-y[ind])
+        x,y,ind = self.normalize(points)
+        yfit = params['slope']*x+params['intercept']
+        return dict(yfit=yfit[ind],residuals=yfit[ind]-y[ind])
 
 class SimpleLinearRegression(LinearRegression):
+
     """
         slr = SimpleLinearRegression(corr_coeff=True)
         points = [(1,2),(2,3),(3,4)]
@@ -47,8 +48,12 @@ class SimpleLinearRegression(LinearRegression):
         return result
 
 class IteratedRobustLinearRegression(LinearRegression):
+
     def __init__(self,**kw):
-        # set parameters, like outlier threshold, etc. see above for example
+	# set parameters, like outlier threshold, etc. see above for example
+	self._stopping_rvalue = kw.get('max_rvalue',0.99)
+	self._poor_linearity_rvalue = kw.get('min_rvalue',0.5)
+	self._maxremoved = kw.get('max_removed',20)
 	super(IteratedRobustLinearRegression,self).__init__(**kw)
 	# initialize simple linear regression we will call repeatedly.
 	self._slr = SimpleLinearRegression()
@@ -56,27 +61,44 @@ class IteratedRobustLinearRegression(LinearRegression):
     def fit(self,points):
 	self.check(points)
 	points1 = copy.copy(points)
+
+	removed = 0
 	while True:
-	    params=self._slr.fit(points1)
-	    result=self._slr.evalute(params,points1)
+	    try:
+	        params=self._slr.fit(points1)
+	    except:
+		raise RuntimeError('fit failed')
+
+	    result=self._slr.evaluate(params,points1)
+	    
 	    # decide whether to stop or not...
 	    # Use r, residuals, etc.
-	    if stop:
+	    if params['r'] > self._stopping_rvalue:
 		break
+	    
 	    # Check it is reasonable to keep going, if not raise RuntimeError("message")
-	    if not resonable:
+	    if params['r'] < self._poor_linearity_rvalue:
 		raise RuntimeError("Can't do the fit!")
-	    # manipulate points1 to remove one pair of values
-	    points1.remove((None,None))
-	
+	  
+	    index = np.argmax(abs(result['residuals']))
+
+	    print points1[index],' is removed!' 
+	    del points1[index]
+
+	    removed += 1
+	    if removed > self._maxremoved:
+                raise RuntimeError('Too many points removed!')
+            if len(result['residuals']) < self._minpoints:
+                raise RuntimeError('Too few points!')
+
 	# Once out of loop, result has values from final SLR fit
-	return result
+	return params
 
 if __name__ == "__main__":
     import sys, os
 
-    lr = SimpleLinearRegression()
-    # lr = IteratedRobustLinearRegression(outlier_threshold=2)
+   # lr = SimpleLinearRegression()
+    lr = IteratedRobustLinearRegression()
     points = [(1,2),(2,3),(3,4)]
     print points
     print lr.normalize(points)
