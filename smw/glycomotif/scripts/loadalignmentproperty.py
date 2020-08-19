29ced9d03dcaa9960a2afdf8174cd72a6eae1290
Hendricks27 <wenjinzhang95@gmail.com>
2020-08-19 15:53:07 -0400

    a few fixes

--- smw/glycomotif/scripts/loadalignmentproperty.py
+++ smw/glycomotif/scripts/loadalignmentproperty.py
@@ -1,8 +1,6 @@
 #!/bin/env python27
 
-import os
 import sys
-
 from getwiki import GlycoMotifWiki
 w = GlycoMotifWiki()
 
@@ -16,11 +14,7 @@ for m in w.itermotif():
     if redend == None:
         redend = []
 
-
-    # CCRC CCRC.000114 G26199BG [True]
-    # print collection, pageid, acc, redend
     # "Substructure" "Core" "Whole" "Nonreducing-End"
-
     alignment_type = []
     if True in redend:
         alignment_type.append("Core")
@@ -31,13 +25,12 @@ for m in w.itermotif():
     if redend == None or len(redend) == 0:
         alignment_type = ["Core", "Substructure"]
 
-    if collection not in ["GGM"]:
+    if collection in ["GGM"]:
 
         try:
-            # print idnum
-            idnum = pageid.split(",")[1]
+            idnum = pageid.split(".")[1]
             idnum = int(idnum)
-            if idnum >=60 and idnum <=122:
+            if idnum >= 60 and idnum <= 122:
                 alignment_type = ["Whole"]
         except:
             pass
@@ -46,8 +39,7 @@ for m in w.itermotif():
         if pageid == "GGM.000034":
             alignment_type = ["Nonreducing-End"]
 
-
-    print "%s set to %s" % (m.get("id"), alignment_type)
+    print >> sys.stderr, "%s set to %s" % (m.get("id"), alignment_type)
     m.set("alignment", alignment_type)
     w.update(m)
 
