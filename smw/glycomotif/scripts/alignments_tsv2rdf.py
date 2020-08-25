512a9564c084e52f24441870746a45c0cb2573db
Hendricks27 <wenjinzhang95@gmail.com>
2020-08-25 00:41:00 -0400

    alignment property

--- smw/glycomotif/scripts/alignments_tsv2rdf.py
+++ smw/glycomotif/scripts/alignments_tsv2rdf.py
@@ -13,10 +13,10 @@ rdf_file_path = sys.argv[3] # "../data/alignment.rdf"
 
 alignments = {}
 alignments_template = {
-    "core": [],
-    "substructure": [],
-    "whole": [],
-    "nred": []
+    "Core": [],
+    "Substructure": [],
+    "Whole": [],
+    "Nonreducing-End": []
 }
 for i, line in enumerate(csv.reader(open(motif_tsv), dialect="excel-tab")):
     if i == 0:
@@ -31,16 +31,16 @@ for i, line in enumerate(csv.reader(open(motif_tsv), dialect="excel-tab")):
         alignments[macc] = copy.deepcopy(alignments_template)
 
     if flags[0]:
-        alignments[macc]["core"].append(gacc)
+        alignments[macc]["Core"].append(gacc)
 
     if flags[1]:
-        alignments[macc]["substructure"].append(gacc)
+        alignments[macc]["Substructure"].append(gacc)
 
     if flags[2]:
-        alignments[macc]["whole"].append(gacc)
+        alignments[macc]["Whole"].append(gacc)
 
     if flags[3]:
-        alignments[macc]["nred"].append(gacc)
+        alignments[macc]["Nonreducing-End"].append(gacc)
 
 """
 for macc, res0 in alignments.items():
@@ -48,9 +48,6 @@ for macc, res0 in alignments.items():
     for alignments_type, res00 in res0.items():
         print alignments_type[:4], res00[:10]
 """
-
-# TODO filter the result based on "alignment" property
-
 
 rdfgraph = rdflib.Graph()
 
@@ -60,8 +57,7 @@ skosns = rdflib.Namespace('http://www.w3.org/2004/02/skos/core#')
 swivtns = rdflib.Namespace('http://semantic-mediawiki.org/swivt/1.0#')
 glycomotifns = rdflib.Namespace('http://glycandata.glygen.org/glycomotif#')
 
-# TODO XML header is slightly different
-# TODO 2 namespaces are not used
+
 rdfgraph.bind("rdf", rdfns)
 rdfgraph.bind("rdfs", rdfsns)
 rdfgraph.bind("glycomotif", glycomotifns)
@@ -69,26 +65,45 @@ rdfgraph.bind("swivt", swivtns)
 rdfgraph.bind("skos", skosns)
 
 abbr_extension = {
-    "core": "Core Alignment",
-    "substructure": "Substructure Alignment",
-    "whole": "Whole-Glycan Alignment",
-    "nred": "Nonreducing-End Alignment"
+    "Core": "Core Alignment",
+    "Substructure": "Substructure Alignment",
+    "Whole": "Whole-Glycan Alignment",
+    "Nonreducing-End": "Nonreducing-End Alignment"
 }
 
+from getwiki import GlycoMotifWiki
+w = GlycoMotifWiki()
 
-for motifacc, alignments_per_motif in alignments.items():
+for m in w.itermotif():
+    pageid = m.get("id")
+    motifacc = m.get("glytoucan")
+    alignment_allowed_type = m.get("alignment")
 
-    motif_rdf_node = rdflib.URIRef("http://glycandata.glygen.org/%s/Special:URIResolver/GM.%s" % (prefix, motifacc))
+    print >> sys.stderr, pageid
+
+    if motifacc not in alignments:
+        print >> sys.stderr, "No alignments found for %s(%s)" % (pageid, motifacc)
+        continue
+
+    alignments_per_motif = alignments[motifacc]
+
+    motif_rdf_node = rdflib.URIRef("http://glycandata.glygen.org/%s/Special:URIResolver/%s" % (prefix, pageid))
     rdfgraph.add((motif_rdf_node, rdfns.type, swivtns["Subject"]))
 
     for alignment_type_abbr, structure_accs in alignments_per_motif.items():
         alignment_type = abbr_extension[alignment_type_abbr]
 
+        if alignment_type_abbr not in alignment_allowed_type:
+            print >> sys.stderr, "Skipping alignment for %s %s" % (pageid, alignment_type)
+            continue
+        print >> sys.stderr, "Loading alignment for %s %s" % (pageid, alignment_type)
+
         for acc in structure_accs:
             tmp = "_".join((alignment_type_abbr, motifacc, acc))
-            alignment_id = hashlib.sha256(tmp).hexdigest()
+            # alignment_id = hashlib.sha256(tmp).hexdigest()
+            alignment_id = "_".join((alignment_type_abbr, pageid, acc))
 
-            matched_rdf_node = glycomotifns["alignment_"+alignment_id]
+            matched_rdf_node = glycomotifns[alignment_id]
             rdfgraph.add((matched_rdf_node, glycomotifns["structure_accession"], rdflib.Literal(acc)))
             rdfgraph.add((matched_rdf_node, glycomotifns["alignment_type"], rdflib.Literal(alignment_type)))
             rdfgraph.add((motif_rdf_node, glycomotifns["has_alignment"], matched_rdf_node))
