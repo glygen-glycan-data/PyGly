3eddc676c35c6eab7b6be6f8458c35e866568233
Nathan Edwards <edwardsnj@gmail.com>
2020-10-23 20:11:19 -0400

    Preparation for new release

--- smw/glycomotif/scripts/loadallmotif.py
+++ smw/glycomotif/scripts/loadallmotif.py
@@ -9,19 +9,21 @@ w = GlycoMotifWiki()
 import findpygly
 from pygly.GlycanResource import GlyTouCan
 
-gtcres = GlyTouCan(usecache=False,prefetch=True,verbose=False)
-
 gtc2motif = defaultdict(list)
 for m in w.itermotif():
+    print(m.get('id'))
     if m.get('collection') != AllMotif.id:
         gtc = m.get('glytoucan')
         gtc2motif[gtc].append(m)
+print('Done')
 
 current = set()
 for gtc,motiflist in sorted(gtc2motif.iteritems()):
+    print gtc
     names = []
     redend = set()
     aglycon = set()
+    alignment = set()
     for m in sorted(motiflist,key=lambda m: (m.get('collection'),m.get('accession'))):
 	if m.has('name'):
 	    for n in m.get('name'):
@@ -31,12 +33,15 @@ for gtc,motiflist in sorted(gtc2motif.iteritems()):
 	    redend.update(m.get('redend',[]))
 	if m.has('aglycon'):
 	    aglycon.update(m.get('aglycon',[]))
+	if m.has('alignment'):
+	    alignment.update(m.get('alignment',[]))
     if len(redend) == 0:
 	redend = None
     if len(aglycon) == 0:
 	aglycon = None
-    motif = AllMotif(accession=gtc,name=names,redend=redend,aglycon=aglycon,
-                     wurcs=gtcres.getseq(gtc,'wurcs'),glycoct=gtcres.getseq(gtc,'glycoct'))
+    if len(alignment) == 0:
+	alignment = None
+    motif = AllMotif(accession=gtc,name=names,redend=redend,aglycon=aglycon,alignment=alignment)
     current.add(gtc)
     if w.update(motif):
 	print >>sys.stderr, gtc
