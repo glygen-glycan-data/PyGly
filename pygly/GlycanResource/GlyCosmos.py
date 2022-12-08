c7a5021e8807d334340d86006041b2e0c48917f1
Nathan Edwards <edwardsnj@gmail.com>
2022-12-08 14:04:01 -0500

    GlyCosmos TripleStore seems to be unreliable

--- pygly/GlycanResource/GlyCosmos.py
+++ pygly/GlycanResource/GlyCosmos.py
@@ -2,6 +2,7 @@
 from .GlyCosmosTS import GlyCosmosTS
 from .GlyTouCanUtil import GlyTouCanUtil
 from .GlyCosmosSparqList import GlyCosmosSparqList
+from .GlyCosmosWS import GlyCosmosWS
 
 import os.path
 
@@ -9,8 +10,10 @@ class GlyCosmos(GlyCosmosTS,GlyTouCanUtil):
     def __init__(self,**kw):
         kw['iniFile'] = os.path.join(os.path.dirname(os.path.realpath(__file__)),"glycosmos.ini")
         super(GlyCosmos,self).__init__(**kw)
-        self.sparqlist = GlyCosmosSparqList()
+        self.sparqlist = GlyCosmosSparqList(**kw)
         self.attach_methods(self.sparqlist)
+	self.ws = GlyCosmosWS(**kw)
+	self.attach_methods(self.ws)
 
 class GlyCosmosNoCache(GlyCosmos):
     def __init__(self,**kw):
---
+++ pygly/GlycanResource/GlyCosmosWS.py
@@ -0,0 +1,59 @@
+
+from .WebServiceResource import WebServiceResource
+
+import os, os.path, re, time
+import hashlib 
+
+class GlyCosmosWS(WebServiceResource):
+    apiurl = 'https://glycosmos.org'
+    export = ['allaccessions','allmass','getmass','getseq']
+
+    def __init__(self,**kw):
+        kw['iniFile'] = os.path.join(os.path.dirname(os.path.realpath(__file__)),"glycosmosws.ini")
+        super(GlyCosmosWS,self).__init__(**kw)
+        self._cache = None
+        self.make_cache()
+
+    def make_cache(self):
+        if self._cache == None:
+            self._cache = dict()
+            for r in self.query_glycans():
+                acc = r['Accession Number']
+                try:
+                    mass = float(r['Monoisotopic mass'])
+	        except:
+                    mass = None
+                self._cache[acc] = dict(acc=acc,mass=mass,wurcs=r['WURCS'],**r)
+
+    def allrows(self):
+        for d in self._cache.values():
+            yield d
+
+    def getrow(self,acc):
+        return self._cache.get(acc)
+
+    def allaccessions(self):
+        for d in self.allrows():
+            yield d['acc']
+
+    def allmass(self):
+        for d in self.allrows():
+            yield d['acc'],d['mass']
+
+    def allseq(self,format='wurcs'):
+        assert(format == 'wurcs')
+        for d in self.allrows():
+            yield d['acc'],d['WURCS']
+
+    def getmass(self,acc):
+        d = self.getrow(acc) 
+        if d:
+            return d['mass']
+        return None
+
+    def getseq(self,acc,format='wurcs'):
+        assert(format == 'wurcs')
+        d = self.getrow(acc) 
+        if d:
+            return d['WURCS']
+        return None
