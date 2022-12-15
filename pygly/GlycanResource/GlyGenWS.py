a58d69fbe9ea3de1b79df62d90701fb45620e092
Nathan Edwards <edwardsnj@gmail.com>
2022-12-15 15:11:32 -0500

    glycan search for glygen

--- pygly/GlycanResource/GlyGenWS.py
+++ pygly/GlycanResource/GlyGenWS.py
@@ -1,7 +1,7 @@
 
 from .WebServiceResource import WebServiceResource
 
-import os, os.path, re, time
+import os, os.path, re, time, json
 import hashlib 
 
 class GlyGenWS(WebServiceResource):
@@ -10,3 +10,13 @@ class GlyGenWS(WebServiceResource):
     def __init__(self,**kw):
         kw['iniFile'] = os.path.join(os.path.dirname(os.path.realpath(__file__)),"glygenws.ini")
         super(GlyGenWS,self).__init__(**kw)
+
+    def glycan_search(self,**kw):
+        query=json.dumps(kw)
+        result = self.query_glycan_search(query=query)
+        # print(result['queryinfo'])
+        return result['results']
+
+    def glycans_bytype(self,type):
+         for d in self.glycan_search(glycan_type=type):
+             yield d['glytoucan_ac']
