d834aeef5ffd70706066a162e986adf784a69874
Nathan Edwards <edwardsnj@gmail.com>
2022-12-16 17:51:29 -0500

    Alterative glygen search strategy seems to work

--- pygly/GlycanResource/GlyGenWS.py
+++ pygly/GlycanResource/GlyGenWS.py
@@ -11,12 +11,20 @@ class GlyGenWS(WebServiceResource):
         kw['iniFile'] = os.path.join(os.path.dirname(os.path.realpath(__file__)),"glygenws.ini")
         super(GlyGenWS,self).__init__(**kw)
 
-    def glycan_search(self,**kw):
+    def glycan_search(self, **kw):
+        query=json.dumps(kw)
+        result = self.query_glycan_search(query=query)
+        list_id = result['list_id']
+        query=json.dumps(dict(id=list_id,type='glycan_list',format='csv',compressed=False))
+        for r in self.query_download_list(query=query):
+            yield r
+
+    def glycan_directsearch(self,**kw):
         offset = 1
         while True:
             query=json.dumps(dict(offset=offset,**kw))
             offset += 1
-            result = self.query_glycan_search(query=query)
+            result = self.query_glycan_directsearch(query=query)
             print(dict(len=len(result['results']),**result['queryinfo']))
             for d in result['results']:
                 yield d
