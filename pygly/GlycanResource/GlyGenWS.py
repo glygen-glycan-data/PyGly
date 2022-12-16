80d766864b00b90a097fb8a574710bf3aaf43c4e
Nathan Edwards <edwardsnj@gmail.com>
2022-12-16 13:24:14 -0500

    this is the approved method, but the API has a bug

--- pygly/GlycanResource/GlyGenWS.py
+++ pygly/GlycanResource/GlyGenWS.py
@@ -12,11 +12,17 @@ class GlyGenWS(WebServiceResource):
         super(GlyGenWS,self).__init__(**kw)
 
     def glycan_search(self,**kw):
-        query=json.dumps(kw)
-        result = self.query_glycan_search(query=query)
-        # print(result['queryinfo'])
-        return result['results']
+        offset = 1
+        while True:
+            query=json.dumps(dict(offset=offset,**kw))
+            offset += 1
+            result = self.query_glycan_search(query=query)
+            print(dict(len=len(result['results']),**result['queryinfo']))
+            for d in result['results']:
+                yield d
+            if len(result['results']) == 0:
+                break
 
     def glycans_bytype(self,type):
-         for d in self.glycan_search(glycan_type=type):
+         for i,d in enumerate(self.glycan_search(glycan_type=type)):
              yield d['glytoucan_ac']
