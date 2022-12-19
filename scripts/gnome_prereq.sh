36f93295b6bc8961cd4712ff9137c535004651fe
Nathan Edwards <edwardsnj@gmail.com>
2022-12-19 11:02:05 -0500

    redo gnome prereq stuff

--- scripts/gnome_prereq.sh
+++ scripts/gnome_prereq.sh
@@ -12,52 +12,44 @@ fi
 
 set -x
 
-# Force us to get from the github master branch (not locally!) to standard out
-# We are running under a cloned copy of PyGly repository, which also
-# contains smw/glycandata, so we can use git show.
-GET="git show origin/master:smw/glycandata/"
+GLYRES="python2 ../pygly/GlycanResource/main.py"
 
-function get() {
-  ${GET}$1
+function restriction () {
+  $GLYRES $1 $2 | sort -u > ./GNOme/restrictions/GNOme_${3}.accessions.txt 
 }
 
-function getprereq () {
-  get $1 > ./GNOme/$2
+function glygentyperes () {
+  $GLYRES GlyGen glycans_bytype $1 > ./GNOme/restrictions/GNOme_${2}.accessions.txt 
 }
 
-function getaccprereq () {
-  get $1 | awk 'NR > 1 {print $1}' | sort -u > ./GNOme/$2
+function sandboxres () {
+  $GLYRES GlycoTreeSandbox list $1 > ./GNOme/restrictions/GNOme_${2}.accessions.txt 
 }
 
-function getclassificationprereq () {
-  get $1 | awk -F'\t' -v glycantype=$3 '$2 == "GlycanType" && $3 == glycantype && NR > 1 {print $1}' | sort -u > ./GNOme/$2
+function glycomotifres () {
+  $GLYRES GlycoMotifNoCache getstruct GGM $1 > ./GNOme/restrictions/GNOme_${2}.accessions.txt 
 }
 
-function getglycotreeprereq () {
-  wget -O - "https://glygen.ccrc.uga.edu/sandbox/api/list.php?par=&mode=${1}" | jq -r '.[] | .glytoucan_ac' | sort -u > ./GNOme/$2
+function getdata () {
+  $GLYRES $1 $2 | bash -c '(read -r; echo "$REPLY"; sort)' > ./GNOme/data/$3
 }
 
 # Restrictions
-getaccprereq export/bcsdb.tsv restrictions/GNOme_BCSDB.accessions.txt
-getprereq data/glygen_accessions.txt restrictions/GNOme_GlyGen.accessions.txt
-getprereq data/glycosmos_allacc.txt restrictions/GNOme_GlyCosmos.accessions.txt
-getclassificationprereq export/classification.tsv restrictions/GNOme_GlyGen_NGlycans.accessions.txt N-linked
-getclassificationprereq export/classification.tsv restrictions/GNOme_GlyGen_OGlycans.accessions.txt O-linked
-getglycotreeprereq "mapped_N" restrictions/GNOme_GlycoTree_NGlycans.accessions.txt
-getglycotreeprereq "mapped_O" restrictions/GNOme_GlycoTree_OGlycans.accessions.txt
+restriction GlyGen allglycans GlyGen
+restriction GlyCosmosNoCache allaccessions GlyCosmos
 
-#N glycan restriction from alignment to N glycan core motif
-curl -s --compressed https://glycomotif.glyomics.org/glycomotif/sparql/query -X POST --data 'query=PREFIX+glycomotif%3A+%3Chttp%3A%2F%2Fglyomics.org%2Fglycomotif%23%3E%0A%0ASELECT+%3Fstructure_gtc_acc%0AWHERE+%7B%0A++++%3Fcollection+a+glycomotif%3ACollection+.%0A++++%3Fcollection+glycomotif%3Aid+%3Fcollectionid+.%0A++++%0A++++%3Fmotif+a+glycomotif%3AMotif+.%0A++++%3Fmotif+glycomotif%3Aaccession+%3Faccession+.%0A++++%3Fmotif+glycomotif%3Aincollection+%3Fcollection+.%0A%0A++++%3Fmotif+glycomotif%3Aglytoucan+%3Fmotif_gtc_acc+.%0A%0A++++%3Falignment+glycomotif%3Amotif_accession+%3Fmotif_gtc_acc+.%0A++++%3Falignment+glycomotif%3Astructure_accession+%3Fstructure_gtc_acc+.%0A%0A++++%3Fmotif+glycomotif%3Aid+%22GGM.001001%22+.%0A%7D' -H 'Accept: application/sparql-results+json' | jq -r '.results.bindings[].structure_gtc_acc.value' | sort -u > ./GNOme/restrictions/GNOme_NGlycans.accessions.txt
+glygentyperes N-linked GlyGen_NGlycans
+glygentyperes O-linked GlyGen_OGlycans
 
-# GNOme raw file
-getprereq data/gnome_subsumption_raw.txt data/gnome_subsumption_raw.txt
+sandboxres mapped_N GlycoTree_NGlycans
+sandboxres mapped_O GlycoTree_OGlycans
+
+glycomotifres 001001 NGlycans
 
 # GlyTouCan retired etc.
-getprereq data/glytoucan_archived.txt data/glytoucan_archived.txt
-getprereq data/glytoucan_replaced.txt data/glytoucan_replaced.txt
+getdata GlyCosmosNoCache archived glytoucan_archived.txt
+getdata GlyCosmosNoCache replaced glytoucan_replaced.txt
 
 # Synonyms
-getprereq data/byonic2glytoucan.txt data/byonic2glytoucan.txt
-getprereq data/shortuckbcomp2glytoucan.txt data/shortuckbcomp2glytoucan.txt
-getprereq data/shortcomp2glytoucan.txt data/shortcomp2glytoucan.txt
-
+python2 ../smw/glycandata/scripts/getbasecomplist.py \* > ./GNOme/data/basecomplist.txt
+( cd ./GNOme/data; ../../../smw/glycandata/data/splitbasecomp.sh )
