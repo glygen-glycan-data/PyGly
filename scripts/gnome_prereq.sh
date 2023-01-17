33cd507f0769b39ceeb013a6419b8e6254c6a310
Nathan Edwards <edwardsnj@gmail.com>
2023-01-17 14:45:55 -0500

    update GNOMe prereq steps

--- scripts/gnome_prereq.sh
+++ scripts/gnome_prereq.sh
@@ -12,37 +12,24 @@ fi
 
 set -x
 
-# Force us to get from the github master branch (not locally!) to standard out
-# We are running under a cloned copy of PyGly repository, which also
-# contains smw/glycandata, so we can use git show.
-GET="git show origin/master:smw/glycandata/"
+GLYRES="python2 ../pygly/GlycanResource/main.py"
 
-function get() {
-  ${GET}$1
+function restriction () {
+  $GLYRES $1 $2 | sort -u > ./GNOme/restrictions/GNOme_${1}.accessions.txt 
 }
 
-function getprereq () {
-  get $1 > ./GNOme/$2
-}
-
-function getaccprereq () {
-  get $1 | awk 'NR > 1 {print $1}' | sort -u > ./GNOme/$2
+function getdata () {
+  $GLYRES $1 $2 | bash -c '(read -r; echo "$REPLY"; sort)' > ./GNOme/data/$3
 }
 
 # Restrictions
-getaccprereq export/bcsdb.tsv restrictions/GNOme_BCSDB.accessions.txt
-getprereq data/glygen_accessions.txt restrictions/GNOme_GlyGen.accessions.txt
-getprereq data/glycosmos_allacc.txt restrictions/GNOme_GlyCosmos.accessions.txt
-
-# GNOme raw file
-getprereq data/gnome_subsumption_raw.txt data/gnome_subsumption_raw.txt
+restriction GlyGen allglycans
+restriction GlyCosmosNoCache allaccessions
 
 # GlyTouCan retired etc.
-getprereq data/glytoucan_archived.txt data/glytoucan_archived.txt
-getprereq data/glytoucan_replaced.txt data/glytoucan_replaced.txt
+getdata GlyCosmosNoCache archived glytoucan_archived.txt
+getdata GlyCosmosNoCache replaced glytoucan_archived.txt
 
 # Synonyms
-getprereq data/byonic2glytoucan.txt data/byonic2glytoucan.txt
-getprereq data/shortuckbcomp2glytoucan.txt data/shortuckbcomp2glytoucan.txt
-getprereq data/shortcomp2glytoucan.txt data/shortcomp2glytoucan.txt
-
+python2 ../smw/glycandata/scripts/getbasecomplist.py \* > ./GNOme/data/basecomplist.txt
+( cd ./GNOme/data; ../../../smw/glycandata/data/splitbasecomp.sh )
