f939eb6417a7d276e6acadb6e5248be2e9f5ec70
Nathan Edwards <edwardsnj@gmail.com>
2023-08-16 16:56:21 -0400

    use glymage images for GlyGen

---
+++ smw/glycandata/scripts/makeimages1.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -x
+
+rm -f images*.zip images*.tbz*
+mkdir -p images/snfg/extended/png
+cat ../data/{glygen,glygen_new,extra}_accessions.txt | awk '{printf("/data/projects/GlyGen/APIFramework/src/Application/Glymage/work/snfg/extended/%s.png\n",$1)}' | xargs -i cp {} images/snfg/extended/png
+mkdir -p images/snfg/extended/svg
+cat ../data/{glygen,glygen_new,extra}_accessions.txt | awk '{printf("/data/projects/GlyGen/APIFramework/src/Application/Glymage/work/snfg/extended/%s.svg\n",$1)}' | xargs -i cp {} images/snfg/extended/svg
+pwd
+
+cd images
+tar cf - snfg/extended/png | bzip2 -c | split -b 40m -d - '../images-snfg-extended-png.tbz.'
+tar cf - snfg/extended/svg | bzip2 -c | split -b 40m -d - '../images-snfg-extended-svg.tbz.'
+echo -e "GlyTouCanAccession\tImage-Size\tImage-Notation\tImage-Style\tImage-Format" > "../images.tsv"
+find snfg/extended \( -name "*.svg" -o -name "*.png" \) -ls | fgrep '/extended/' | awk '{print $7,$11}' | tr '/.' '  ' | awk '{print $5,$1,$2,$3,$4}' | sort -k1,1 -k3,3 -k4,4 -k5,5 | tr ' ' '\t' >> "../images.tsv"
