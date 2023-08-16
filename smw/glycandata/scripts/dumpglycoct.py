c5b1928066dde83bf138077b080df51712aae346
Nathan Edwards <edwardsnj@gmail.com>
2023-08-16 16:51:39 -0400

    table of canonical residue ids for glycoTree structures

--- smw/glycandata/scripts/dumpglycoct.py
+++ smw/glycandata/scripts/dumpglycoct.py
@@ -1,13 +1,26 @@
 #!/bin/env python2
 
-import sys, time, traceback, hashlib, os, os.path, glob
+import sys, time, traceback, hashlib, os, os.path, glob, csv
 from collections import defaultdict
 
 import findpygly
+from pygly.alignment import GlycanEqual
+from pygly.GlycanFormatter import GlycoCTFormat, WURCS20Format, GlycanParseError
+
 from getwiki import GlycanData
 w = GlycanData()
 
+wp = WURCS20Format()
+gp = GlycoCTFormat()
+glyeq = GlycanEqual()
+
 allfn = set(glob.glob(sys.argv[1]+"/G*.txt"))
+
+idmapfilename = sys.argv[1] + "/residmap.txt"
+idmaps = defaultdict(dict)
+if os.path.exists(idmapfilename):
+    for r in csv.DictReader(open(idmapfilename),dialect='excel-tab'):
+        idmaps[r['Accession']][int(r['GlycoCTResidueIndex'])] = int(r['CanonicalResidueIndex'])
 
 glycanclass = sys.argv[2]
 assert glycanclass in ("N-linked","O-linked")
@@ -18,6 +31,10 @@ for g in w.iterglycan():
     gct = None
     if g.has_annotations(property='GlycoCT',type='Sequence'):
 	gct = g.get_annotation_value(property='GlycoCT',type='Sequence')
+
+    wcs = None
+    if g.has_annotations(property='WURCS',type='Sequence'):
+	wcs = g.get_annotation_value(property='WURCS',type='Sequence')
 
     inclass = False
     if glycanclass == "N-linked":
@@ -39,6 +56,23 @@ for g in w.iterglycan():
     if gct == None or not inclass:
 	continue
 
+    try:
+        gctgly = gp.toGlycan(gct)
+        wcsgly = wp.toGlycan(wcs)
+    except GlycanParseError:
+        continue
+    except:
+        traceback.print_exc()
+        print acc
+        sys.exit(1)
+
+    idmap = []
+    if not glyeq.eq(gctgly,wcsgly,idmap=idmap):
+	continue
+
+    for gctmono,wcsmono in idmap:
+        idmaps[acc][int(gctmono.id())] = int(wcsmono.external_descriptor_id())
+
     filename = sys.argv[1] + "/" + acc + ".txt"
 
     if filename in allfn:
@@ -51,5 +85,14 @@ for g in w.iterglycan():
     wh.close()
 
 for fn in allfn:
+    acc = os.path.split(fn)[1][:-4]
     print >>sys.stderr, "Removing:",acc
     os.unlink(fn)
+    del idmaps[acc]
+
+wh = open(idmapfilename,'w')
+print >>wh, "\t".join(map(str,["Accession","GlycoCTResidueIndex","CanonicalResidueIndex"]))
+for acc in sorted(idmaps):
+    for gctid in sorted(idmaps[acc]):
+        print >>wh, "\t".join(map(str,[acc,gctid,idmaps[acc][gctid]]))
+wh.close()
--- smw/glycandata/scripts/glycoTree_exports.sh
+++ smw/glycandata/scripts/glycoTree_exports.sh
@@ -4,5 +4,5 @@ GLYCOTREE="$2"
 EXPORT="../export"
 ./glycoTree.sh "$SMW" "$GLYCOTREE"
 rm -f $EXPORT/glycotree*.zip
-zip -j $EXPORT/glycotree_nlinked_gct.zip $GLYCOTREE/data/glygengct_nlinked/G*.txt
-zip -j $EXPORT/glycotree_olinked_gct.zip $GLYCOTREE/data/glygengct_olinked/G*.txt
+zip -j $EXPORT/glycotree_nlinked_gct.zip $GLYCOTREE/data/glygengct_nlinked/G*.txt $GLYCOTREE/data/glygengct_nlinked/residmap.txt
+zip -j $EXPORT/glycotree_olinked_gct.zip $GLYCOTREE/data/glygengct_olinked/G*.txt $GLYCOTREE/data/glygengct_olinked/residmap.txt
