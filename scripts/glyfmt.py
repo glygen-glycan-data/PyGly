bc18456643744c84702968a4676c8458066c9e95
Nathan Edwards <edwardsnj@gmail.com>
2023-09-14 10:58:59 -0400

    extend glyfmt script to support compositions and iupac strings

--- scripts/glyfmt.py
+++ scripts/glyfmt.py
@@ -3,9 +3,10 @@ import sys, os.path
 import findpygly
 from pygly.GlycanFormatter import *
 from pygly.GlycanBuilderSVGParser import *
+from pygly.CompositionFormatter import *
 
-if sys.argv[1] not in ("glycoct","wurcs","svg"):
-    print("Parser should be one of: glycoct, wurcs, svg.")
+if sys.argv[1] not in ("glycoct","wurcs","svg","comp","iupac"):
+    print("Parser should be one of: glycoct, wurcs, svg, comp, iupac.")
     exit(1)
 if sys.argv[1] == "glycoct":
     clsname = "GlycoCTFormat"
@@ -13,6 +14,10 @@ elif sys.argv[1] == "wurcs":
     clsname = "WURCS20Format"
 elif sys.argv[1] == "svg":
     clsname = "GlycanBuilderSVG"
+elif sys.argv[1] == "comp":
+    clsname = "CompositionFormat"
+elif sys.argv[1] == "iupac":
+    clsname = "IUPACLinearFormat"
 
 clsinst = eval("%s()"%(clsname,))
 if len(sys.argv) > 2:
@@ -26,7 +31,7 @@ for f in files:
         g = clsinst.toGlycan(seq)
         print("+++", os.path.split(f)[1])
         # print(GlycoCTFormat().toXML(g))
-        print(GlycoCTFormat().toStr(g))
+        # print(GlycoCTFormat().toStr(g))
         for m in g.all_nodes(undet_subst=True):
             print(m)
         if not g.repeated():
@@ -37,6 +42,4 @@ for f in files:
         print("!!!", os.path.split(f)[1], e)
         print(e)
         bad += 1
-    except KeyError:
-        pass
 print("Failed: %d/%d"%(bad,len(files)))
