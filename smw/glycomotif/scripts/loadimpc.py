df3651f9c959155c663bdf42b4d6c1fea4d7e8bb
Nathan Edwards <edwardsnj@gmail.com>
2023-10-10 13:41:31 -0400

    augment phenotype annotations with source

--- smw/glycomotif/scripts/loadimpc.py
+++ smw/glycomotif/scripts/loadimpc.py
@@ -7,15 +7,21 @@ from collections import defaultdict
 w = GlycoMotifWiki()
 
 phenotypes = defaultdict(list)
+mgiacc = dict()
 # "Gene Symbol","MGI Gene Id","# Phenotype Hits","Phenotype Hits"
 for row in csv.DictReader(gzip.open(sys.argv[1])):
-    phenotypes[row['Gene Symbol']].extend([p.strip() for p in row["Phenotype Hits"].split("::")])
+    phenotypes[row['Gene Symbol']].extend(filter(None,[p.strip() for p in row["Phenotype Hits"].split("::")]))
+    mgiacc[row['Gene Symbol']] = row["MGI Gene Id"]
 
 for e in w.iterenzyme():
     gn = e.get('genename')
     if len(phenotypes.get(gn,[])) > 0:
        e.set("phenotype",sorted(phenotypes[gn]))
+       e.set("phenotype_source","IMPC")
+       e.set("phenotype_source_key",mgiacc[gn])
     else:
        e.delete("phenotype")
+       e.delete("phenotype_source")
+       e.delete("phenotype_source_key")
     if w.put(e):
         print gn
