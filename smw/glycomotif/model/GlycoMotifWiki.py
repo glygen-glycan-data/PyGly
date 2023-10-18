99970e1181bcd9b2d70475961d3d19e7b58a02ad
Nathan Edwards <edwardsnj@gmail.com>
2023-10-18 12:16:20 -0400

    add additional list key-value pairs

--- smw/glycomotif/model/GlycoMotifWiki.py
+++ smw/glycomotif/model/GlycoMotifWiki.py
@@ -105,8 +105,26 @@ class Motif(SMW.SMWClass):
 	if isinstance(data.get('keyword'),basestring):
 	    data['keyword'] = set(map(lambda s: s.strip(),data.get('keyword').split(';')))
 
+	if isinstance(data.get('sandbox'),basestring):
+	    data['sandbox'] = set(map(lambda s: s.strip(),data.get('sandbox').split(';')))
+
+	if isinstance(data.get('sandbox_nlinked'),basestring):
+	    data['sandbox_nlinked'] = set(map(lambda s: s.strip(),data.get('sandbox_nlinked').split(';')))
+
+	if isinstance(data.get('sandbox_olinked'),basestring):
+	    data['sandbox_olinked'] = set(map(lambda s: s.strip(),data.get('sandbox_olinked').split(';')))
+
 	if isinstance(data.get('enzyme'),basestring):
 	    data['enzyme'] = set(map(lambda s: s.strip(),data.get('enzyme').split(';')))
+
+	if isinstance(data.get('humanenzyme'),basestring):
+	    data['humanenzyme'] = set(map(lambda s: s.strip(),data.get('humanenzyme').split(';')))
+
+	if isinstance(data.get('mouseenzyme'),basestring):
+	    data['mouseenzyme'] = set(map(lambda s: s.strip(),data.get('mouseenzyme').split(';')))
+
+	if isinstance(data.get('motifcanonres'),basestring):
+	    data['motifcanonres'] = set(map(lambda s: s.strip(),data.get('motifcanonres').split(';')))
 
 	if isinstance(data.get('dbxref'),basestring):
 	    data['dbxref'] = set(map(lambda s: tuple(s.strip().split(':')),data.get('dbxref').split(';')))
@@ -132,6 +150,24 @@ class Motif(SMW.SMWClass):
 
         if 'enzyme' in data:
             data['enzyme'] = ";".join(filter(None,sorted(set(map(lambda s: s.strip(),data['enzyme'])))))
+
+        if 'humanenzyme' in data:
+            data['humanenzyme'] = ";".join(filter(None,sorted(set(map(lambda s: s.strip(),data['humanenzyme'])))))
+
+        if 'mouseenzyme' in data:
+            data['mouseenzyme'] = ";".join(filter(None,sorted(set(map(lambda s: s.strip(),data['mouseenzyme'])))))
+
+        if 'motifcanonres' in data:
+            data['motifcanonres'] = ";".join(filter(None,sorted(set(map(lambda s: s.strip(),data['motifcanonres'])))))
+
+        if 'sandbox' in data:
+            data['sandbox'] = ";".join(filter(None,sorted(set(map(lambda s: s.strip(),data['sandbox'])))))
+
+        if 'sandbox_nlinked' in data:
+            data['sandbox_nlinked'] = ";".join(filter(None,sorted(set(map(lambda s: s.strip(),data['sandbox_nlinked'])))))
+
+        if 'sandbox_olinked' in data:
+            data['sandbox_olinked'] = ";".join(filter(None,sorted(set(map(lambda s: s.strip(),data['sandbox_olinked'])))))
 
         if 'dbxref' in data:
             data['dbxref'] = ";".join(map(lambda t: "%s:%s"%t,sorted(data['dbxref'])))
@@ -293,6 +329,9 @@ class Enzyme(SMW.SMWClass):
 	if isinstance(data.get('phenotype'),basestring):
 	    data['phenotype'] = data.get('phenotype','').split(';')
 
+	if isinstance(data.get('disease'),basestring):
+	    data['disease'] = data.get('disease','').split(';')
+
 	if isinstance(data.get('ortholog'),basestring):
 	    data['ortholog'] = data.get('ortholog','').split(',')
 
@@ -303,6 +342,9 @@ class Enzyme(SMW.SMWClass):
 
 	if 'phenotype' in data:
 	    data['phenotype'] = ';'.join(map(str,data.get('phenotype')))
+
+	if 'disease' in data:
+	    data['disease'] = ';'.join(map(str,data.get('disease')))
 
         if 'ortholog' in data:
             data['ortholog'] = ",".join(sorted(data['ortholog']))
