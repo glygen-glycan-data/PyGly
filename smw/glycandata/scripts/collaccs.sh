0b4b5716d4c289e4e38122d8974d339274305b3a
Nathan Edwards <edwardsnj@gmail.com>
2024-10-01 15:13:53 -0400

    update collaccs.sh to use glyres and python3.12

--- smw/glycandata/scripts/collaccs.sh
+++ smw/glycandata/scripts/collaccs.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 set -x
 
-PYTHON=python3
-PYTHON2=python2
-PYTHON3=python3
-PYGLY=../../../pygly
+GLYRES=../../../scripts/glyres.py
 DATA=../data
 
-$PYTHON $PYGLY/GlycanResource/main.py GlyGen allglycans | sort > $DATA/glygen_accessions.txt
+$GLYRES GlyGen allglycans | sort > $DATA/glygen_accessions.txt
 
 touch $DATA/glygen_exclude_accessions.txt
 for accfile in $DATA/glygen_accessions.txt; do
@@ -25,30 +22,28 @@ cat $DATA/glygen_manual_accessions.txt >> $DATA/glygen_req_accessions.txt
 
 # All GlyTouCan motifs...
 # echo "#GlycoMotif allmotifs GGM" >> $DATA/glygen_req_accessions.txt
-$PYTHON $PYGLY/GlycanResource/main.py GlycoMotifNoCache allmotifs GGM | awk '{print $2}' >> $DATA/glygen_req_accessions.txt
+$GLYRES GlycoMotifNoCache allmotifs GGM | awk '{print $2}' >> $DATA/glygen_req_accessions.txt
 
-for taxid in 9606 10090 10116 10114 111108 11116 11103 3052230 63746 694009 2697049 7227 4932 9823 44689 9031 3702; do
-  # echo "#GlyTouCanNoCache bytaxa $taxid" >> $DATA/glygen_req_accessions.txt
-  $PYTHON $PYGLY/GlycanResource/main.py GlyTouCanNoCache bytaxa $taxid >> $DATA/glygen_req_accessions.txt
-  $PYTHON $PYGLY/GlycanResource/main.py GlyCosmosNoCache bytaxa $taxid >> $DATA/glygen_req_accessions.txt
+for taxid in 9606 10090 10116 10114 111108 11116 11103 3052230 63746 694009 2697049 7227 4932 9823 44689 9031 3702 9913; do
+  $GLYRES GlyTouCanNoCache bytaxa $taxid >> $DATA/glygen_req_accessions.txt
+  $GLYRES GlyCosmosNoCache bytaxa $taxid >> $DATA/glygen_req_accessions.txt
 done
 
-# echo "#GlyGenSourceFile allgtc" >> $DATA/glygen_req_accessions.txt
-$PYTHON $PYGLY/GlycanResource/main.py GlyGenSourceFile allsourcegtc | awk '{print $2}' >> $DATA/glygen_req_accessions.txt
+$GLYRES GlyGenSourceFile allsourcegtc | awk '{print $2}' >> $DATA/glygen_req_accessions.txt
 
 cat $DATA/glygen_req_accessions.txt | \
   fgrep -v -f $DATA/glygen_accessions.txt | \
     sort -u > $DATA/glygen_new_accessions.txt
 
-$PYTHON get_extra_accessions.py $DATA/gnome_subsumption_raw.txt $DATA/glygen_accessions.txt $DATA/glygen_new_accessions.txt | sort > $DATA/extra_accessions.txt
+./get_extra_accessions.py $DATA/gnome_subsumption_raw.txt $DATA/glygen_accessions.txt $DATA/glygen_new_accessions.txt | sort > $DATA/extra_accessions.txt
 
-( $PYTHON ./getbasecomplist.py \* > $DATA/basecomplist.txt ) > $DATA/basecomplist.log 
+( ./getbasecomplist.py \* > $DATA/basecomplist.txt ) > $DATA/basecomplist.log 
 ( cd $DATA; ./splitbasecomp.sh )
 ./uckbdata.sh
-$PYTHON $PYGLY/GlycanResource/main.py GlyCosmosNoCache replaced | bash -c '(read -r; echo "$REPLY"; sort)' > $DATA/glytoucan_replaced.txt
-$PYTHON $PYGLY/GlycanResource/main.py GlyCosmosNoCache archived | bash -c '(read -r; echo "$REPLY"; sort)' > $DATA/glytoucan_archived.txt
-$PYTHON $PYGLY/GlycanResource/main.py GlyTouCanNoCache allaccessions | sort > $DATA/glytoucan_allacc.txt
-$PYTHON $PYGLY/GlycanResource/main.py GlyCosmosNoCache allaccessions | sort > $DATA/glycosmos_allacc.txt
+$GLYRES GlyCosmosNoCache replaced | bash -c '(read -r; echo "$REPLY"; sort)' > $DATA/glytoucan_replaced.txt
+$GLYRES GlyCosmosNoCache archived | bash -c '(read -r; echo "$REPLY"; sort)' > $DATA/glytoucan_archived.txt
+$GLYRES GlyTouCanNoCache allaccessions | sort > $DATA/glytoucan_allacc.txt
+$GLYRES GlyCosmosNoCache allaccessions | sort > $DATA/glycosmos_allacc.txt
 
 for accfile in $DATA/glygen_accessions.txt $DATA/glygen_new_accessions.txt $DATA/extra_accessions.txt; do
   fgrep -v -f $DATA/glytoucan_archived.txt $accfile > ${accfile}.new; mv -f ${accfile}.new $accfile
