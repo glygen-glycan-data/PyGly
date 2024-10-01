12e68efee96b6804942c1c2e17ef9e1c6b04d112
Nathan Edwards <edwardsnj@gmail.com>
2024-10-01 15:13:10 -0400

    new glycotree export file w/ caveats

--- smw/glycandata/scripts/glycoTree_exports.sh
+++ smw/glycandata/scripts/glycoTree_exports.sh
@@ -4,4 +4,5 @@ EXPORT="../export"
 set -x
 cp $GLYCOTREE/accessions.lst $EXPORT/glycotree_accessions.tsv
 cp $GLYCOTREE/glycotree_annotated_glycans.tsv.gz $EXPORT
+cp $GLYCOTREE/glycotree_glycan_caveats.tsv.gz $EXPORT
 gzip -c $GLYCOTREE/portal/api/paths/allPaths.json > $EXPORT/glycotree_allpaths.json.gz
