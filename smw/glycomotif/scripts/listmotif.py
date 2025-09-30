a458f2b09063e1a010af565a57fe23417ae3b8f1
Nathan Edwards <edwardsnj@gmail.com>
2025-09-30 11:16:46 -0400

    update for classes

--- smw/glycomotif/scripts/listmotif.py
+++ smw/glycomotif/scripts/listmotif.py
@@ -1,9 +1,36 @@
-#!/bin/env python27
+#!/bin/env python3.12
 
-from getwiki import GlycoMotifWiki, GlyTouCanMotif
+from getwiki import GlycoMotifWiki, GlyTouCanMotif, Class
+import sys
 
 w = GlycoMotifWiki()
 
-for m in w.itermotif(collection='GGM'):
-    print m
-    # print m.get('collection'), m.get('accession'), m.get('id')
+from clseng import ClassifierEngine
+
+ce = ClassifierEngine()
+
+for cl in ce.classifiers():
+    theid = cl._id
+    wcl = w.get(cl._id)
+    if not wcl:
+        wcl = Class(id=cl._id)
+    wcl.update(hasmotif=cl._motifs,nothasmotif=cl._exceptions,name=cl._class[-1],
+               level=("Type" if len(cl._class) == 1 else "Subtype"))
+    if w.put(wcl):
+        print(cl._id)
+
+sys.exit()
+for cl in w.iterpages(regex=r'^GGC\.'):
+    print(w.get(cl.name))
+sys.exit()
+
+for r in w.itermotifgtcalign(regex=r'^GGM\.001'):
+    print(r)
+sys.exit()
+
+for m in w.itermotif(regex='GGM.001'):
+    keywords = set(m.get('keyword',[]))
+    keywords.add("Glycan classification")
+    m.set('keyword',keywords)
+    if w.put(m):
+        print(m.get('id'))
